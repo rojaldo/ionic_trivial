6ec00c0a495ba651b3cd06a18453cd8d5bc1c1fb
Daniel Rojo <danielrojo@gmail.com>
2018-06-22 20:25:17 +0200

    some fixes

--- src/app/model/card.ts
+++ src/app/model/card.ts
@@ -19,8 +19,17 @@ export class TrivialCard {
         this.answers = new Array<string>();
         this.answers = data.incorrect_answers;
         this.answers.push(this.correctAnswer);
+        this.answers = this.shuffle(this.answers);
         this.answered = false;
         this.correct = false;
         this.indexAnswer = -1;
+    }
+
+    shuffle(a: Array<string>): string[] {
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+        }
+        return a;
     }
 }
